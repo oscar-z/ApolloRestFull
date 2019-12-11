6109bb686f7eb38852cf4877f7eeca5ec8920508
Oscar <oscar.ims93@gmail.com>
2019-12-10 22:04:42 -0600

    url change

--- client/src/config/createApolloClient.js
+++ client/src/config/createApolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-    uri: 'https://apollo-app-oscar-z.herokuapp.com/'
+    uri: 'https://apollo-app-oscar-z.herokuapp.com/graphql'
 });
 
 export default client;
